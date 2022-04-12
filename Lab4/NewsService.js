6ea7fea39054c67d71338e06768c5d9bc2bdf0f4
Chris <burd573@gmail.com>
2022-04-11 18:43:41 -0700

    Finish requests on activity 1

---
+++ Lab4/NewsService.js
@@ -0,0 +1,221 @@
+import { readFileSync, writeFileSync } from "fs";
+
+// Set the date of the new story to the current day and format date
+const setDate = () => {
+  var date = new Date();
+  var year = date.getFullYear();
+
+  var month = (date.getMonth() + 1).toString();
+  month = month.length > 1 ? month : "0" + month;
+
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : "0" + day;
+
+  let ret = month + "-" + day + "-" + year;
+  return ret;
+};
+
+var dataFile = "";
+
+// News story object, can be modified to add new story
+const story = {
+  TITLE: "",
+  AUTHOR: "",
+  DATE: setDate(),
+  PUBLIC: "",
+  CONTENT: "",
+};
+
+// Data retrieved from file, contains all news stories stored in file
+const data = {
+  contents: "data",
+  articles: [],
+  selected: "",
+};
+
+// Filters author and title from news stories
+const textFilter = {
+  AUTHOR: "",
+  TITLE: "",
+};
+
+// sets date range to filter from news stories
+const dateFilter = {
+  startDate: "",
+  endDate: setDate(),
+};
+
+// Loads news stories data from file
+export const loadData = () => {
+  data.contents = JSON.parse(readFileSync(dataFile));
+  data.articles = data.contents.NEWS.ARTICLE;
+};
+
+/*************** Update Functions ************************/
+// Writes a new news story to the file
+export const newStory = (input, output) => {
+  const newData = data.articles.push(input);
+  writeFileSync(output, JSON.stringify(data.contents));
+};
+
+export const addStory = (title, author, publicFlag, content) => {
+  loadData();
+  setTitle(title);
+  setAuthor(author);
+  setPublic(publicFlag);
+  setContent(content);
+  data.contents.NEWS.ARTICLE.push(story);
+
+  writeFileSync(dataFile, JSON.stringify(data.contents));
+};
+
+export const getStory = (selected) => {
+  loadData();
+  return data.articles[selected];
+};
+
+export const setFile = (input) => {
+  dataFile = input;
+};
+
+// Set the title of the new story
+const setTitle = (input) => {
+  story.TITLE = input;
+};
+
+// Set the author of the new story
+const setAuthor = (input) => {
+  story.AUTHOR = input;
+};
+
+// Set the public flag of the new story
+const setPublic = (input) => {
+  story.PUBLIC = input;
+};
+
+// Set the content of the new story
+const setContent = (input) => {
+  story.CONTENT = input;
+};
+
+const setAuthorFilter = (authorFilter) => {
+  textFilter.AUTHOR = authorFilter;
+};
+
+const setTitleFilter = (titleFilter) => {
+  textFilter.TITLE = titleFilter;
+};
+
+const setStartDateFilter = (startDateFilter) => {
+  dateFilter.startDate = startDateFilter;
+};
+
+const setEndDateFilter = (endDateFilter) => {
+  dateFilter.endDate = endDateFilter;
+};
+
+// Updates news story headline
+export const updateHeadline = (index, newHeadline) => {
+  loadData();
+  data.articles[index].TITLE = newHeadline;
+  writeFileSync(dataFile, JSON.stringify(data.contents));
+};
+
+// Updates news story content
+export const updateContent = (index, newContent) => {
+  loadData();
+  data.articles[index].CONTENT = newContent;
+  writeFileSync(dataFile, JSON.stringify(data.contents));
+};
+
+// Deletes a news story from the file
+export const deleteStory = (index) => {
+  loadData();
+  data.articles.splice(index);
+  writeFileSync(dataFile, JSON.stringify(data.contents));
+};
+
+/*************** Filter Text Functions ************************/
+// Sets the title and author filters
+export const setFilters = (title, author) => {
+  if (title != undefined) {
+    textFilter.TITLE = title;
+  }
+  if (author != undefined) {
+    textFilter.AUTHOR = author;
+  }
+};
+
+// Clear only title filter
+const clearTitleFilter = () => {
+  textFilter.TITLE = "";
+};
+
+// clear only author filter
+const clearAuthorFilter = () => {
+  textFilter.AUTHOR = "";
+};
+
+// Filter all stories by title and author, returns filtered array of stories
+export const filterStoriesText = () => {
+  loadData();
+  let stories = data.articles.filter(function (story) {
+    for (var key in textFilter) {
+      if (!story[key].toLowerCase().includes(textFilter[key].toLowerCase())) {
+        return false;
+      }
+    }
+    return true;
+  });
+  return stories;
+};
+
+/*************** Filter Date Functions ************************/
+// Set the date filter range
+export const setDateFilter = (startDate, endDate) => {
+  if (startDate != undefined) {
+    dateFilter.startDate = startDate;
+  }
+  if (endDate != undefined) {
+    dateFilter.endDate = endDate;
+  }
+};
+
+// Check if article is in range of dates provided by date filter
+const inRange = (date, startRange, endRange) => {
+  let storyDate = new Date(date);
+  let StartDate = new Date(startRange);
+  let endDate = new Date(endRange);
+
+  return storyDate >= StartDate && storyDate <= endDate;
+};
+
+// Filter stories by date, takes in array of stories
+export const filterStoriesDate = (input) => {
+  loadData();
+  if (dateFilter.startDate !== "" && dateFilter.endDate !== "") {
+    let stories = input.filter(function (story) {
+      if (!inRange(story.DATE, dateFilter.startDate, dateFilter.endDate)) {
+        return false;
+      }
+      return true;
+    });
+    return stories;
+  } else {
+    return input;
+  }
+};
+
+// filter stories on all criteria (author, title, date range)
+export const filter = () => {
+  var ret = filterStoriesText();
+  return filterStoriesDate(ret);
+};
+
+// Clear all filters
+export const clearFilters = () => {
+  textFilter.TITLE = "";
+  textFilter.AUTHOR = "";
+  dateFilter.startDate = "";
+  dateFilter.endDate = setDate();
+};
---
+++ Lab4/activity1/NewsAPI.js
@@ -0,0 +1,58 @@
+import * as newsService from "../NewsService.js";
+import express from "express";
+// import bodyParser from "body-parser";
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(3001);
+
+const file = "newsStories/news.json";
+
+app.get("/stories/:author?/:title?/:startDate?/:endDate?", function (req, res) {
+  newsService.setFile(file);
+  newsService.setFilters(req.query.title, req.query.author);
+  newsService.setDateFilter(req.query.startDate, req.query.endDate);
+  let filtered = newsService.filter();
+  newsService.clearFilters();
+  res.set("Content-Type", "application/json");
+  res.send(filtered);
+});
+
+app.post("/updateHeadline", function (req, res) {
+  newsService.setFile(file);
+  newsService.updateHeadline(req.body.selected, req.body.headline);
+  res.set("Content-Type", "application/json");
+  res.send(
+    "Updated Title: " + JSON.stringify(newsService.getStory(req.body.selected))
+  );
+});
+
+app.post("/updateContent", function (req, res) {
+  newsService.setFile(file);
+  newsService.updateContent(req.body.selected, req.body.content);
+  res.set("Content-Type", "application/json");
+  res.send(
+    "Updated Content: " +
+      JSON.stringify(newsService.getStory(req.body.selected))
+  );
+});
+
+app.post("/deleteStory", function (req, res) {
+  newsService.setFile(file);
+  let deleted = newsService.getStory(req.body.selected);
+  newsService.deleteStory(req.body.selected);
+  res.set("Content-Type", "application/json");
+  res.send("Story deleted: " + JSON.stringify(deleted));
+});
+
+app.post("/addStory", function (req, res) {
+  newsService.setFile(file);
+  newsService.addStory(
+    req.body.title,
+    req.body.author,
+    req.body.public,
+    req.body.content
+  );
+  res.set("Content-Type", "application/json");
+  res.send("Story Added: " + JSON.stringify(req.body));
+});
